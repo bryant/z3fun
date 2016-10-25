4180c51299a66aeba67703a44dd22a3afb8ca8fb
bryant <bryant@users.noreply.github.com>
2016-10-25 08:17:18 -0400

    more

--- src/Z3Fun.hs
+++ src/Z3Fun.hs
@@ -1,15 +1,18 @@
-{-# LANGUAGE GADTs, KindSignatures, DataKinds #-}
+{-# LANGUAGE GADTs, KindSignatures, DataKinds, ScopedTypeVariables #-}
+{-# LANGUAGE FlexibleInstances #-}
 
 module Z3Fun where
 
-import GHC.TypeLits (KnownNat, Nat)
-import Control.Monad.Trans.State (State)
+import GHC.TypeLits (KnownNat, Nat, natVal)
+import Control.Monad.Trans.State (State, get, modify, runState)
+import Data.Word (Word64)
+import Data.Proxy (Proxy(..))
 
 data BitVec (n :: Nat)
 
 data AST :: * -> * where
     Var :: Int -> AST a  -- variable id
-    BitConst :: KnownNat n => Integer -> AST (BitVec n)
+    BitConst :: KnownNat n => Word64 -> AST (BitVec n)
     BoolConst :: Bool -> AST Bool
     BitBinOp :: KnownNat n => BitBinOp -> AST (BitVec n) -> AST (BitVec n)
              -> AST (BitVec n)
@@ -18,7 +21,7 @@ data AST :: * -> * where
              -> AST Bool
     BoolBinOp :: BoolBinOp -> AST Bool -> AST Bool -> AST Bool
     BoolUnOp :: BoolUnOp -> AST Bool -> AST Bool
-    TernOp :: AST Bool -> AST t -> AST t -> AST t
+    Ite :: AST Bool -> AST t -> AST t -> AST t  -- if-then-else
 
 data BitCmp
     = BitUlt
@@ -41,6 +44,11 @@ data BitBinOp
     | BitShl
     | BitLShr
     | BitAShr
+    | BitAnd
+    | BitOr
+    | BitXor
+    | BitNor
+    | BitNand
 
 data BitUnOp = BitNeg | BitNot
 
@@ -53,15 +61,19 @@ data BoolBinOp
 
 data BoolUnOp = BoolNot
 
-data Z3Env = Z3Env { var_id :: Int }
+data Z3Env
+    = Z3Env
+    { preconditions :: [AST Bool]
+    , var_id :: Int
+    }
 
 type Z3 = State Z3Env
 
 next_var :: Z3 Int
-next_var = var_id <$> get <* modify (\(Z3Env i) -> Z3Env $ i + 1)
+next_var = var_id <$> get <* modify (\z -> z { var_id = var_id z + 1})
 
 run_z3 :: Z3 a -> a
-run_z3 = fst . flip runState (Z3Env 0)
+run_z3 = fst . flip runState (Z3Env [] 0)
 
 word :: Z3 (AST (BitVec n))
 word = Var <$> next_var
@@ -80,6 +92,78 @@ word64 = word
 
 word128 :: Z3 (AST (BitVec 128))
 word128 = word
+
+class ToSMT t where to_smt :: t -> String
+
+instance KnownNat n => ToSMT (AST (BitVec n)) where
+    to_smt (Var n) = "x_" ++ show n
+    to_smt (BitConst c)
+        | nat_max < fromIntegral c = error msg
+        | otherwise = terms ["_", "bv" ++ show c, show size]
+        where
+        size = fromIntegral $ natVal (Proxy :: Proxy n) :: Word64
+        nat_max = 2 ^ (fromIntegral size :: Integer) - 1 :: Integer
+        msg = "const " ++ show c ++ " is too large for BitVec " ++ show size
+    to_smt (BitBinOp op lhs rhs) = term3 op lhs rhs
+    to_smt (BitUnOp op t) = term2 op t
+    to_smt (Ite cond t f) = terms ["ite", to_smt cond, to_smt t, to_smt f]
+
+instance ToSMT (AST Bool) where
+    to_smt (Var n) = "x_" ++ show n
+    to_smt (BoolConst n) = if n then "true" else "false"
+    to_smt (BitCmp cmp lhs rhs) = term3 cmp lhs rhs
+    to_smt (BoolBinOp op lhs rhs) = term3 op lhs rhs
+    to_smt (BoolUnOp op t) = term2 op t
+    to_smt (Ite cond t f) = terms ["ite", to_smt cond, to_smt t, to_smt f]
+
+instance ToSMT BitBinOp where
+    to_smt BitAdd  = "bvadd"
+    to_smt BitSub  = "bvsub"
+    to_smt BitMul  = "bvmul"
+    to_smt BitURem = "bvurem"
+    to_smt BitSRem = "bvsrem"
+    to_smt BitSMod = "bvsmod"
+    to_smt BitShl  = "bvshl"
+    to_smt BitLShr = "bvlshr"
+    to_smt BitAShr = "bvashr"
+    to_smt BitAnd  = "bvand"
+    to_smt BitOr   = "bvor"
+    to_smt BitXor  = "bvxor"
+    to_smt BitNor  = "bvnor"
+    to_smt BitNand = "bvnand"
+
+instance ToSMT BitUnOp where
+    to_smt BitNeg = "bvneg"
+    to_smt BitNot = "bvnot"
+
+instance ToSMT BoolUnOp where
+    to_smt BoolNot = "not"
+
+instance ToSMT BoolBinOp where
+    to_smt BoolAnd = "and"
+    to_smt BoolOr  = "or"
+    to_smt BoolXor = "xor"
+    to_smt BoolImp = "=>"
+    to_smt BoolEq  = "="
+
+instance ToSMT BitCmp where
+    to_smt BitUlt = "bvult"
+    to_smt BitUgt = "bvugt"
+    to_smt BitUle = "bvule"
+    to_smt BitUge = "bvuge"
+    to_smt BitSlt = "bvslt"
+    to_smt BitSgt = "bvsgt"
+    to_smt BitSle = "bvsle"
+    to_smt BitSge = "bvsge"
+    to_smt BitEq  = "="
+
+terms xs = "(" ++ unwords xs ++ ")"
+
+term2 :: (ToSMT a, ToSMT b) => a -> b -> String
+term2 a b = terms [to_smt a, to_smt b]
+
+term3 :: (ToSMT a, ToSMT b, ToSMT c) => a -> b -> c -> String
+term3 a b c = terms [to_smt a, to_smt b, to_smt c]
 
 {-
 main = do
