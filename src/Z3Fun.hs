a9ebc03b81b3b15a3de5c00a95c90e6dd188ec38
bryant <bryant@users.noreply.github.com>
2016-10-25 07:26:08 -0400

    add ast for bitvec and bools

---
+++ src/Z3Fun.hs
@@ -0,0 +1,65 @@
+{-# LANGUAGE GADTs, KindSignatures, DataKinds #-}
+
+module Z3Fun where
+
+import GHC.TypeLits
+import Control.Monad.Trans.State
+
+data BitVec (n :: Nat)
+
+data AST :: * -> * where
+    Var :: Int -> AST a  -- variable id
+    BitConst :: KnownNat n => Integer -> AST (BitVec n)
+    BoolConst :: Bool -> AST Bool
+    BitBinOp :: KnownNat n => BitBinOp -> AST (BitVec n) -> AST (BitVec n)
+             -> AST (BitVec n)
+    BitUnOp :: KnownNat n => BitBinOp -> AST (BitVec n) -> AST (BitVec n)
+    BitCmp :: KnownNat n => BitCmp -> AST (BitVec n) -> AST (BitVec n)
+             -> AST Bool
+    BoolBinOp :: BoolBinOp -> AST Bool -> AST Bool -> AST Bool
+    BoolUnOp :: BoolUnOp -> AST Bool -> AST Bool
+    TernOp :: AST Bool -> AST t -> AST t -> AST t
+
+data BitCmp
+    = BitUlt
+    | BitUgt
+    | BitUle
+    | BitUge
+    | BitSlt
+    | BitSgt
+    | BitSle
+    | BitSge
+    | BitEq  -- equality
+
+data BitBinOp
+    = BitAdd
+    | BitSub
+    | BitMul
+    | BitURem
+    | BitSRem
+    | BitSMod
+    | BitShl
+    | BitLShr
+    | BitAShr
+
+data BitUnOp = BitNeg | BitNot
+
+data BoolBinOp
+    = BoolAnd
+    | BoolOr
+    | BoolXor
+    | BoolImp  -- ^ implication
+    | BoolEq  -- ^ logical equivalence
+
+data BoolUnOp = BoolNot
+
+data Z3Env = Z3Env { var_id :: Int }
+
+{-
+main = do
+    prove f
+    where
+    f x c0 c1 = c0 > 0 && x == (x << c0) >> c0 ==>
+                (x << c0) > c1 <==> x > (c1 >> c0)
+        where (>) = ugt
+-}
